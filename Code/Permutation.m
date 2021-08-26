e8e5d3c488c86bc36574c7af6b52b1cda99ad7e4
RMDE <435869851@qq.com>
2021-08-26 17:48:45 +0800

    adding the permutation process after the adjustment process

--- Code/Permutation.m
+++ Code/Permutation.m
@@ -3,22 +3,22 @@
 %origin: the image after the adjustment process
 %blocksize: the width of the block
 %key: key for generating the random order for permutation
-%NUM: in order to distinguish between the two distribution of areas, 0 for type 1 and 1 for type 0
+%edge: in order to distinguish between the two distribution of areas, 0 for type 1 and others for type 0
 
-function PmImage = Permutation(origin, blocksize, key, NUM)
+function PmImage = Permutation(origin, blocksize, key, edge)
     LIMIT = 1000;
     rng('default');
     rng(key);
     PmImage = origin;
     [M,N,C] = size(origin); 
-    m = (M - NUM*2)/blocksize;
-    n = (N - NUM*2)/blocksize;
+    m = (M - edge*2)/blocksize;
+    n = (N - edge*2)/blocksize;
     keys = uint8(randi(LIMIT,m,n));
     for chanal = 1 : 1 : C
         for i = 1 : m
             for j = 1 : n
-                x = (i-1)*blocksize+1+NUM;
-                y = (j-1)*blocksize+1+NUM;
+                x = (i-1)*blocksize+1+edge;
+                y = (j-1)*blocksize+1+edge;
                 sub(:,:) = origin(x:x+blocksize-1, y:y+blocksize-1, chanal);
                 sub = Permutate(sub, blocksize, keys(i,j));
                 PmImage(x:x+blocksize-1, y:y+blocksize-1, chanal) = sub(:, :);
